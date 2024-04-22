0b5091d10cf5cc750409844cede6cb332636eb6f
dalia61 <eng.dalia61@gmail.com>
2024-04-22 18:01:57 +0200

    CreateXcodeProject

---
+++ KooraLive/KooraLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KooraLive
+//
+//  Created by Dalia on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
